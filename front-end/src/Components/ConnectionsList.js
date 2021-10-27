2876356e20f4ddd107b3970f50ae2a83b899a263
Coreen Cooper <coreencooper@pursuit.org>
2021-10-26 23:18:37 -0400

    comment out all frontend styling

--- front-end/src/Components/ConnectionsList.js
+++ front-end/src/Components/ConnectionsList.js
@@ -2,42 +2,68 @@ import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../Providers/UserProvider";
 import { apiURL } from "../util/apiURL";
 import axios from "axios";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const API = apiURL();
 
 const ConnectionsList = () => {
   const [friendsList, setFriendsList] = useState([]);
+  const [displayList, setDisplayList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const user = useContext(UserContext);
-  
+
   const fetchList = async () => {
     try {
       const res = await axios.get(`${API}/users/${user.uid}/connections`);
-      setFriendsList(res.data);
+      setFriendsList((_friendsList) => {
+        setDisplayList(res.data);
+        return res.data
+      });
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    setDisplayList((_) => {
+      return [...friendsList].filter(
+        (friend) =>
+          friend.display_name
+            .toLowerCase()
+            .indexOf(e.target.value.toLowerCase()) > -1
+      );
+    });
   };
 
   useEffect(() => {
     fetchList();
   }, [user]);
 
-
   const deleteConnection = async (friendId) => {
     try {
-        await axios.delete(`${API}/users/${user.uid}/connections/${friendId}`)
-       
-        setFriendsList(friendsList.filter(({user1_id, user2_id}) =>{
-          return (user1_id  !== friendId) && (user2_id !== friendId)
-        }))
+      await axios.delete(`${API}/users/${user.uid}/connections/${friendId}`);
+
+      setFriendsList(
+        friendsList.filter(({ user1_id, user2_id }) => {
+          return user1_id !== friendId && user2_id !== friendId;
+        })
+      );
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}
- 
+  };
+
   return (
     <>
+      <label htmlFor="search-bar"></label>
+      <input
+        className="search-bar"
+        name="search-bar"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
       <table>
         <thead>
           <tr>
@@ -50,20 +76,36 @@ const ConnectionsList = () => {
         </thead>
 
         <tbody>
-          {friendsList ? (
+          {displayList ? (
             <>
-              {friendsList.map((friend, idx) => {
-                const {id, display_name, linkedin, email, twitter, phone_number, uid} = friend 
+              {displayList.map((friend, idx) => {
+                const {
+                  id,
+                  display_name,
+                  linkedin,
+                  email,
+                  twitter,
+                  phone_number,
+                  uid,
+                } = friend;
                 return (
                   <tr key={idx} to={`/connections/${id}`}>
                     <td>{display_name}</td>
-                    <td><a href={`mailto:${email}`}>{email}</a></td>
+                    <td>
+                      <a href={`mailto:${email}`}>{email}</a>
+                    </td>
                     <td>
                       <a href={linkedin}>{linkedin}</a>
                     </td>
-                    <td><a href={twitter}>{twitter}</a></td>
-                    <td><a href={`tel:${phone_number}`}>{phone_number}</a></td>
-                    <td><button onClick={()=>deleteConnection(uid)}>X</button></td>
+                    <td>
+                      <a href={twitter}>{twitter}</a>
+                    </td>
+                    <td>
+                      <a href={`tel:${phone_number}`}>{phone_number}</a>
+                    </td>
+                    <td>
+                      <button onClick={() => deleteConnection(uid)}>X</button>
+                    </td>
                   </tr>
                 );
               })}
