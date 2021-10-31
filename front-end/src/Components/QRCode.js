ac91f532cfe4791f185f73ffbe35745966fdcf1a
Coreen Cooper <coreencooper@pursuit.org>
2021-10-31 12:40:34 -0400

    move generateQR to useEffect()

--- front-end/src/Components/QRCode.js
+++ front-end/src/Components/QRCode.js
@@ -1,54 +1,50 @@
-import  QRCode from "qrcode"
+import QRCode from "qrcode";
 import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../Providers/UserProvider";
 import { apiURL } from "../util/apiURL";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const API = apiURL();
 
 const QrCode = () => {
-    const [src, setSrc] = useState('')
-    const user = useContext(UserContext);
+  const [src, setSrc] = useState("");
+  const user = useContext(UserContext);
 
-    useEffect(() => {
-        generateQR()
-    }, [])
-    
-    const generateQR = async ()=>{
-        
-        try {
-            QRCode.toDataURL(`${API}/users/${user.uid}/card`).then((data) =>{
-                setSrc(data)
-            })
-            
-        } catch (error) {
-         console.log(error)   
-        }
-    }
-    return (
-      <section>
-        <h1>QrCode</h1>
-        <div className="cardDiv">
-          <div className="card">
-            <hr />
-            <Link to="/external">
-                <button>
-                    <img
-                    alt="source"
-                    src ={src} />
-                </button>            
-            </Link>
+  useEffect(() => {
+    const generateQR = async () => {
+      try {
+        QRCode.toDataURL(`${API}/users/${user.uid}/card`).then((data) => {
+          setSrc(data);
+        });
+      } catch (error) {
+        return error;
+      }
+    };
+    generateQR();
+  }, [user.uid]);
 
-            <hr />
-            <Link to="/dashboard">
-                <button className="QRCodeBack">
-                    <i class="fas fa-chevron-circle-left"></i>
-                </button>            
-            </Link>
-          </div>
+  return (
+    <section>
+      <h1>QrCode</h1>
+      <div className="cardDiv">
+        <div className="card">
+          <hr />
+          <Link to="/external">
+            <button>
+              <img alt="source" src={src} />
+            </button>
+          </Link>
+
+          <hr />
+          <Link to="/dashboard">
+            <button className="QRCodeBack">
+              <i class="fas fa-chevron-circle-left"></i>
+            </button>
+          </Link>
         </div>
-      </section>
-    );
-}
+      </div>
+    </section>
+  );
+};
 
 export default QrCode;
