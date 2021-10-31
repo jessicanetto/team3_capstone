b487a1a57f3c016e43e1dec864b4a61cb337ca09
Coreen Cooper <coreencooper@pursuit.org>
2021-10-31 10:45:54 -0400

    update title

--- front-end/src/Pages/Connections.js
+++ front-end/src/Pages/Connections.js
@@ -3,7 +3,7 @@ import ConnectionsList from "../Components/ConnectionsList.js";
 export const Connections = () => {
   return (
     <section className="connections">
-      <h1>Connections Pages</h1>
+      <h1>Connections</h1>
       <ConnectionsList />
     </section>
   );
