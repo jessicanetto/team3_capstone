d322ae318703fbe37a7ba101af65cf16889bc382
Coreen Cooper <coreencooper@pursuit.org>
2021-10-31 09:24:18 -0400

    corrected typo

--- front-end/src/Components/Card.js
+++ front-end/src/Components/Card.js
@@ -58,28 +58,28 @@ render(){
     
     <input
     className = 'name'
-    placeHolder = {this.state.name}
+    placeholder = {this.state.name}
     type = 'text'
     onChange = {this.handleChangeName}
     />
        
     <input
     className = 'phone'
-    placeHolder = {this.state.phoneNumber}
+    placeholder = {this.state.phoneNumber}
     type = 'text'
     onChange = {this.handleChangePhoneNumber}
     />
 
     <input
     className = 'eMail'
-    placeHolder = {this.state.email}
+    placeholder = {this.state.email}
     type = 'text'
     onChange = {this.handleChangeEmail}
     />
 
     <input
     className = 'webSite'
-    placeHolder = {this.state.linkedin}
+    placeholder = {this.state.linkedin}
     type = 'text'
     onChange = {this.handleChangelinkedin}
     />
